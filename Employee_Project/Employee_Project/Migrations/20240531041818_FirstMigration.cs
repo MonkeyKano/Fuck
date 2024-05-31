0706ad0eb0957dc3ac0f97a5f6f7743ad27ae484
2020PECCS261 <2020peccs261@pec.in>
2024-05-31 11:08:28 +0530

    Dummy Dhaya

---
+++ Employee_Project/Employee_Project/Migrations/20240531041818_FirstMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Employee_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Employee_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Employee_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Employee_Salary = table.Column<int>(type: "int", nullable: false),
+                    Employee_Age = table.Column<int>(type: "int", nullable: false),
+                    Profile_Img = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Employee_Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Employee_Project/Employee_Project/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Project.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Employee_Id { get; set; }
+        public string Employee_Name { get; set; }
+        public int Employee_Salary { get; set; }
+        public int Employee_Age { get; set; }
+        public string Profile_Img { get; set; }
+    }
+}
